ff15b965f51608cf2901d8e478a417126e817310
acidic <aiden.l.jones10@gmail.com>
2026-01-09 14:11:00 -0700

    adadad

--- py-fest/py-fest/py_fest.py
+++ py-fest/py-fest/py_fest.py
@@ -1,1 +1,23 @@
+# The initial lineup
+lineup = [
+    ("Code Play", "Indie", 30),
+    ("The Pythonistas", "Rock", 45),
+    ("Syntax Error", "Metal", 60)
+]
 
+# 1. Add the headliner
+headliner = ("The Byte Beats", "Synthwave", 90)
+lineup.append(headliner)
+
+# 2. remove the dudes and put it to the end
+rb = lineup.pop(0)
+lineup.append(rb)
+
+# 3. remove the lame band
+lineup.remove(("The Pythonistas", "Rock", 45))
+
+# 4. calculate total duration
+total_duration = sum(t for _, _, t in lineup)
+
+print(lineup)
+print(total_duration)
